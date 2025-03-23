857118bc766a05a72db65958fcfd959110ab27ca
Unai Mengual <git@owln.ai>
2025-03-23 18:56:18 +0100

    initialize Nuxt 3 project with basic configuration and structure

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  modules: ['@nuxthub/core', 
+    '@nuxt/ui-pro', 
+    '@nuxt/content']
+})
